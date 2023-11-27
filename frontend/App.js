ed39887167146ced84b7c85ef56051642c607b13
kmgalla5 <kmgalla5@ncsu.edu>
2023-11-27 14:43:16 -0500

    Set up basic framework of frontend

---
+++ frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App(props) {
+    return(
+        <div className="App">
+            <h1>"DND Database"</h1>
+        </div>
+    );
+}
+
+export default App;
