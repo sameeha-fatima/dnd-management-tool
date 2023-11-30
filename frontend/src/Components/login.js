a503c61b20fff6b4320d2c404b6d958983bf13e0
kmgalla5 <kmgalla5@ncsu.edu>
2023-11-30 00:44:38 -0500

    Created the frontend for the Login screen

--- frontend/src/Components/login.js
+++ frontend/src/Components/login.js
@@ -1,23 +1,84 @@
 import React from 'react';
 
-const useStyles = ({
-    loginContainer: {
-        height: '100%',
-        width: '65%'
-    },
-    title: {
-        textAlign: 'center',
-        paddingTop: '50px'
-    }
-});
+import styled from 'styled-components';
+
+const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: 65% auto;
+    height: 585px;
+    border: solid;
+`;
+
+const Title = styled.h1`
+    text-align: center;
+    padding-top: 50px;
+    font-family: Garamond, Georgia, serif;
+    font-size: 40px;
+`;
+
+const LoginButton = styled.button`
+    color: white;
+    background-color: blue;
+    text-align: center;
+    margin: 10px 5px;
+    padding-left: 50px;
+    padding-right: 50px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    font-size: medium;
+`;
+
+const LoginLabel = styled.label`
+    font-size: small;
+    padding-right: 150px;
+`;
+
+const LoginInput = styled.input` font-size: medium; `;
+
+const LoginForm = styled.form` text-align: center; `;
+
+const NUBackground = styled.div` 
+    background-color: lightgrey;
+    text-align: center;
+`;
+
+const NUTitle = styled.h1`
+    text-align: center;
+    padding-top: 100px;
+    font-family: Garamond, Georgia, serif;
+    font-size: 40px;
+`;
+
+const NUButton = styled.button`
+    color: white;
+    background-color: #2fb9e6;
+    text-align: center;
+    margin: 10px 5px;
+    padding-left: 50px;
+    padding-right: 50px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    font-size: medium;
+`;
 
 function Login(props) {
-    const style = useStyles();
     return(
-        <div className={style.loginContainer}>
-            <h1 className={style.title}>Label</h1>
-
-        </div>
+        <GridContainer>
+            <div>
+                <Title>Login</Title>
+                <LoginForm action="/action_page.php">
+                    <LoginLabel for="username">Username</LoginLabel><br></br>
+                    <LoginInput id="username" name="username" placeholder="username"></LoginInput><br></br><br></br>
+                    <LoginLabel for="password">Password</LoginLabel><br></br>
+                    <LoginInput id="password" name="password" placeholder="password"></LoginInput><br></br><br></br>
+                    <LoginButton type="submit">Login</LoginButton>
+                </LoginForm>
+            </div>
+            <NUBackground>
+                <NUTitle>New User?</NUTitle>
+                <NUButton>Create Account</NUButton>
+            </NUBackground>
+        </GridContainer>
     );
 }
 
