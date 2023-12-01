d178baaf911e6dcf21001ed15f3c171e5cae2bf3
sfatima3 <sfatima3@ncsu.edu>
2023-12-01 01:00:18 -0500

    Added classes to app.py
    Added classes to app.py

---
+++ app.py
@@ -0,0 +1,206 @@
+# pip install Flask-MySQLdb
+# pip install mysql-connector-python
+
+from flask import Flask
+from flask_mysqldb import MySQL
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = ''
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = ''
+
+mysql = MySQL(app)
+
+def getConnection():
+    return mysql.connector.connect(
+        host = app.config['MYSQL_HOST'],
+        user = app.config['MYSQL_USER'],
+        password = app.config['MYSQL_PASSWORD'],
+        database = app.config['MYSQL_DB']
+    )
+
+class User:
+    def __init__(this, user_id, first_name, last_name, username, password):
+        this.user_id = user_id
+        this.first_name = first_name
+        this.last_name = last_name
+        this.username = username
+        this.password = password
+
+class Session:
+    def __init__(this, session_id, user_id):
+        this.session_id = session_id
+        this.user_id = user_id
+
+class Town:
+    def __init__(this, town_id, town_name, session_id):
+        this.town_id = town_id
+        this.town_name = town_name
+        this.session_id = session_id
+
+class Stat:
+    def __init__(this, stat_id, strength, dexterity, constitution, intelligence, wisdom, charisma):
+        this.stat_id = stat_id
+        this.strength = strength
+        this.dexterity = dexterity
+        this.constitution = constitution
+        this.intelligence = intelligence
+        this.wisdom = wisdom
+        this.charisma = charisma
+
+class Character:
+    def __init__(this, character_id, character_name, job, race, session_id, stat_id):
+        this.character_id = character_id
+        this.character_name = character_name
+        this.job = job
+        this.race = race
+        this.session_id = session_id
+        this.stat_id = stat_id
+
+class CharacterTown:
+    def __init__(this, character_town_id, town_id, character_id):
+        this.character_town_id = character_town_id
+        this.town_id = town_id
+        this.character_id = character_id
+
+class Monster:
+    def __init__(this, monster_id, monster_name, session_id, stat_id):
+        this.monster_id = monster_id
+        this.monster_name = monster_name
+        this.session_id = session_id
+        this.stat_id = stat_id
+
+class Player:
+    def __init__(this, player_id, _class, alignment, character_id):
+        this.player_id = player_id
+        this._class = _class
+        this.alignment = alignment
+        this.character_id = character_id
+
+class Attack:
+    def __init__(this, attack_id, attack_name, damage, session_id):
+        this.attack_id = attack_id
+        this.attack_name = attack_name
+        this.damage = damage
+        this.session_id = session_id
+
+class PlayerAttack:
+    def __init__(this, player_attack_id, attack_id, player_id):
+        this.player_attack_id = player_attack_id
+        this.attack_id = attack_id
+        this.player_id = player_id
+
+class MonsterAttack:
+    def __init__(this, monster_attack_id, attack_id, monster_id):
+        this.monster_attack_id = monster_attack_id
+        this.attack_id = attack_id
+        this.monster_id = monster_id
+
+def create_user(first_name, last_name, username, password):
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO User(FirstName, LastName, Username, Password) VALUES (%s, %s, %s, %s)', (first_name, last_name, username, password))
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def get_all_users():
+    connection = getConnection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM User')
+    users_records = cursor.fetchall()
+    users = [User(**record) for record in users_records]
+    cursor.close()
+    connection.close()
+    return users
+
+def create_session(session_name, user_id):
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO Session (SessionName, UserID) VALUES (%s, %s)', (session_name, user_id))
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def get_all_sessions():
+    connection = getConnection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM Session')
+    session_records = cursor.fetchall()
+    session = [Session(**record) for record in session_records]
+    cursor.close()
+    connection.close()
+    return session
+
+def create_town(town_name, session_id):
+    connection = getConnection()
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO Town (TownName, SessionID) VALUES (%s, %s)', (town_name, session_id))
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+def get_all_towns():
+    connection = getConnection()
+
+def create_stat():
+    connection = getConnection()
+
+def get_all_stats():
+    connection = getConnection()
+
+def create_character():
+    connection = getConnection()
+
+def get_all_characters():
+    connection = getConnection()
+
+def create_character_town():
+    connection = getConnection()
+
+def get_character_town(character_town_id):
+    connection = getConnection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM CharacterTown WHERE CharacterTownID = %s', (character_town_id,))
+    character_town = cursor.fetchone()
+    cursor.close()
+    connection.close()
+
+    if character_town:
+        return CharacterTown(**character_town)
+    else:
+        return None
+
+def create_monster():
+    connection = getConnection()
+
+def get_all_monsters():
+    connection = getConnection()
+
+def create_player():
+    connection = getConnection()
+
+def get_all_players():
+    connection = getConnection()
+
+def create_attack():
+    connection = getConnection()
+
+def get_all_attacks():
+    connection = getConnection()
+
+def create_player_attack():
+    connection = getConnection()
+
+def get_all_player_attacks():
+    connection = getConnection()
+
+def create_monster_attack():
+    connection = getConnection()
+
+def get_all_monster_attacks():
+    connection = getConnection()
