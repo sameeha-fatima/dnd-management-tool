6955354bafc6fe9b4218ba36b016c8c904f4dea8
jigreen <jigreen@ncsu.edu>
2023-12-05 18:14:52 -0500

    Added the delete triggers

--- database/database.sql
+++ database/database.sql
@@ -89,3 +89,164 @@ CREATE TABLE MonsterAttack (
     FOREIGN KEY (AttackID) REFERENCES Attack(AttackID),
     FOREIGN KEY (MonsterID) REFERENCES Monster(MonsterID)
 );
+
+-- Check if username already exists before creating a user
+DELIMITER //
+CREATE PROCEDURE UsernameExists(
+    IN _user_name varchar(32),
+    OUT _result INT
+)
+BEGIN
+    DECLARE existing_count INT;
+    SELECT COUNT(*) INTO existing_count
+    FROM User
+    WHERE Username = _user_name;
+
+    IF existing_count > 0 THEN
+        SET _result = 0;
+    ELSE
+        SET _result = 1;
+    END IF;
+END; 
+//
+DELIMITER ;
+
+--Check if session name already exists, if it does, do not create session
+DELIMITER //
+CREATE PROCEDURE CreateSession(
+    IN _session_id INT,
+    IN _session_name varchar(32),
+    IN _user_id INT,
+    OUT _result INT
+)
+BEGIN
+    DECLARE _duplicate_name BOOLEAN DEFAULT FALSE;
+    SELECT TRUE INTO _duplicate_name
+    FROM Session
+    WHERE SessionName = _session_name AND UserID = _user_id;
+
+    IF _duplicate_name THEN
+        SET _result = 0;
+    ELSE:
+        SET _result = 1;
+    END IF;
+END;
+//
+DELIMITER ;
+
+-- Before deleting player, delete the rows that are associated with player
+DELIMETER //
+CREATE TRIGGER BeforeDeletingPlayer
+BEFORE DELETE ON Player 
+FOR EACH ROW
+BEGIN
+    DECLARE character_id INT;
+    DECLARE stat_id INT;
+    DECLARE attack_id INT;
+
+    SELECT CharacterID, StatID
+    INTO character_id, stat_id
+    FROM Character
+    WHERE PlayerID = OLD.PlayerID;
+
+    DELETE FROM PlayerAttack
+    WHERE AttackID = (SELECT AttackID FROM PlayerAttack WHERE PlayerID = player_id)
+
+    DELETE FROM Character
+    WHERE CharacterID = character_id
+
+    DELETE FROM Stat
+    WHERE StatID = stat_id
+END;
+//
+DELIMETER ;
+
+-- Before deleting a character, delete the rows that are associated with character
+DELIMETER //
+CREATE TRIGGER BeforeDeletingCharacter
+BEFORE DELETE
+ON Character FOR EACH ROW
+BEGIN
+    DECLARE stat_id INT;
+
+    SELECT StatID
+    INTO stat_id
+    FROM Stat
+    Where StatID = OLD.StatID;
+
+    DELETE FROM Stat
+    WHERE StatID = stat_id;
+END;
+//
+DELIMETER ;
+
+
+
+-- Before deleting monster, delete the rows that are associated with monster
+DELIMETER //
+CREATE TRIGGER BeforeDeletingMonster
+BEFORE DELETE ON Monster 
+FOR EACH ROW
+BEGIN
+    DECLARE monster_id INT;
+    DECLARE stat_id INT;
+    DECLARE monster_attack_id INT;
+
+    SELECT MonsterID, StatID
+    INTO monster_id, stat_id
+    FROM Monster
+    WHERE MonsterID = OLD.MonsterID;
+
+    DELETE FROM MonsterAttack
+    WHERE AttackID = (SELECT AttackID FROM MonsterAttack WHERE MonsterID = monster_id);
+
+    DELETE FROM Stat
+    WHERE StatID = stat_id;
+END;
+//
+DELIMETER ;
+
+
+
+-- Before deleting Attack, delete the rows that are associated with attack
+DELIMETER //
+CREATE TRIGGER BeforeDeletingAttack
+BEFORE DELETE ON Attack 
+FOR EACH ROW
+BEGIN
+    DECLARE attack_id INT;
+
+    SELECT AttackID
+    INTO attack_id
+    FROM Attack
+    WHERE AttackID = OLD.AttackID;
+
+    DELETE FROM MonsterAttack
+    WHERE AttackID = attack_id;
+
+    DELETE FROM PlayerAttack
+    WHERE AttackID = attack_id;
+
+END;
+//
+DELIMETER ;
+
+-- Before deleting town, delete the rows that are associated with town
+DELIMETER //
+CREATE TRIGGER BeforeDeletingTown
+BEFORE DELETE ON Town 
+FOR EACH ROW
+BEGIN
+
+    DECLARE town_id INT;
+
+    SELECT TownID INTO town_id
+    FROM Town
+    WHERE TownID = OLD.TownID;
+
+    DELETE FROM CharacterTown
+    WHERE TownID = town_id;
+
+END;
+//
+DELIMETER ;
