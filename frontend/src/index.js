ca4d55a2a78ec7770284195d3bf4f68dc41b2683
John Brain <jtbrain@ncsu.edu>
2023-12-04 20:50:30 -0500

    router and page redirection example

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,38 @@ import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
 
+import Login from './Components/login';
+import CreateAccount from './Components/auth/createAccount/createAccount';
+import SessionLayout from './Components/Session/SessionOverview/Layout/SessionLayout';
+import SessionControl from './Components/sessionControl';
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login></Login>
+    },
+    {
+        path: "/createAccount",
+        element: <CreateAccount></CreateAccount>
+    },
+    {
+        path: "/session/:sessionId",
+        element: <SessionLayout></SessionLayout>
+    },
+    {
+        path: "/sessionControl",
+        element: <SessionControl></SessionControl>
+    },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
-    
 );
